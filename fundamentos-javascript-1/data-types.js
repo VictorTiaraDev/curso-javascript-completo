ab82ce49639ae7b434cedf78b7578387fe0ccbc5
Victor Tiara <victor.tiara@gmail.com>
2024-03-24 12:20:13 -0300

    data types

---
+++ fundamentos-javascript-1/data-types.js
@@ -0,0 +1,11 @@
+let isSland = false;
+let language;
+let population = 210;
+let country = "Brazil";
+
+
+console.log(typeof isSland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof null);
+console.log(typeof language);
