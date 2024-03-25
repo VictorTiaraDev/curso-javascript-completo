c596603d179b60c8065a9938de86661768e0dc4b
Victor Tiara <victor.tiara@gmail.com>
2024-03-25 17:32:17 -0300

    challenger #1

--- fundamentos-javascript-1/challenger#1.js
+++ fundamentos-javascript-1/challenger#1.js
@@ -1,13 +1,13 @@
 
-//1 and 2- TEST'S
+//CHALLENGER #1
 
-let massMark = 78;
-let heightMark = 1.69;
-let massjohn = 92;
-let heightjohn = 1.95;
+const massMark = 78;
+const heightMark = 1.69;
+const massjohn = 92;
+const heightjohn = 1.95;
 
-BMIMark = massMark / (heightMark * heightMark);
-BMIJhon = massjohn / (heightjohn * heightjohn);
-markHigherBMI = BMIMark > BMIJhon;
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJhon = massjohn / (heightjohn * heightjohn);
+let markHigherBMI = BMIMark > BMIJhon;
 
 console.log('BMI of Mark: ' + BMIMark + ' BMI of John: ' + BMIJhon, markHigherBMI + ', Marks BMI is higher.');
