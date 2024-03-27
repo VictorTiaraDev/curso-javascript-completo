d2c688d338bd1913a38427058a6c69f0f17338ee
Victor Tiara <victor.tiara@gmail.com>
2024-03-27 14:46:19 -0300

    type conversion and coercion

---
+++ fundamentos-javascript-1/type-conversion-coercion.js
@@ -0,0 +1,7 @@
+//1- Test
+
+console.log('9' - '5'); // = 4
+console.log('19' - '13' + '17'); // = 617
+console.log('19' - '13' + 17); // = 23
+console.log('123' < 57); // = false;
+console.log(5 + 6 + '4' + 9 - 4 - 2); // = 1143
