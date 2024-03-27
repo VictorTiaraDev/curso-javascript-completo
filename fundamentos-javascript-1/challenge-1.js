6948a89e0fe37a7654124202cb83b71fbaba0756
Victor Tiara <victor.tiara@gmail.com>
2024-03-27 14:48:57 -0300

    challenge-1

--- fundamentos-javascript-1/challenge-1.js
+++ fundamentos-javascript-1/challenge-1.js
@@ -1,9 +1,10 @@
 //CHALLENGER #1
 
-let massMark = 78;
-const heightMark = 1.69;
-let massjohn = 92;
 const heightjohn = 1.95;
+const heightMark = 1.69;
+let massMark = 78;
+let massjohn = 92;
+
 
 let BMIMark = massMark / (heightMark * heightMark);
 let BMIJhon = massjohn / (heightjohn * heightjohn);
