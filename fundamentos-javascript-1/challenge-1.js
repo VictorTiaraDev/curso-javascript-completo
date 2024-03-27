1ac6943c07bfc765734160014a3402d02748bad8
Victor Tiara <victor.tiara@gmail.com>
2024-03-26 21:59:43 -0300

    challenge-1

---
+++ fundamentos-javascript-1/challenge-1.js
@@ -0,0 +1,12 @@
+//CHALLENGER #1
+
+const massMark = 78;
+const heightMark = 1.69;
+const massjohn = 92;
+const heightjohn = 1.95;
+
+let BMIMark = massMark / (heightMark * heightMark);
+let BMIJhon = massjohn / (heightjohn * heightjohn);
+let markHigherBMI = BMIMark > BMIJhon;
+
+console.log('BMI of Mark: ' + BMIMark + ' BMI of John: ' + BMIJhon, markHigherBMI + ', Marks BMI is higher.');
--- fundamentos-javascript-1/string-and-template-literails.js
+++ fundamentos-javascript-1/string-and-template-literails.js
@@ -6,31 +6,40 @@ const continent = "Europe";
 let population = 11;
 
 halfPopulation = population / 2;
-console.log("A metade da população da é: ", halfPopulation);
+
+console.log("A metade da população é: ", halfPopulation);
 
 //2- Increase the population of your country by 1 and log the result to the console.
 
 population = population + 1;
+
 console.log("Acrescentando + 1 na população o resultado é: ", population);
 
 //3- Finland has a population of 6 million. Does your country have more people than Finland?
 
 let populationFiland = 6;
 result1 = population > populationFiland;
+
 console.log("A população de Portugal é maior que a população da Finlandia? ", result1);
 
 //4- The average population of a country is 33 million people. Does you country have less people than the average country?
 
 let populationCountry = 33;
 result2 = population > populationCountry;
+
 console.log("A população de Portugal é maior que a populção de outro País? ", result2);
 
 //5- Based on the variables you created, create a new variable description which contains a string with this format: 
 //'Portugal is in Europe, and its 11 million people speak portuguese'.
 
-population = population - 1;
+/*population = population - 1;
 let sintese = country + ' is in ' + continent + ', and its ' + population + ' million people speak portuguese.';
-console.log(sintese);
 
+console.log(sintese);
+*/
 //6- Recreate the description variable from the last assignment, this time using the template literal syntax.
 
+population = population - 1;
+let sintese = `${country} is in ${continent}, and its ${population} million people speak portuguese.`;
+
+console.log(sintese);
