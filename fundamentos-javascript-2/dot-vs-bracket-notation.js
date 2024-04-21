535e2a6e4817e63839a84d2d3834637b5be473ff
Victor Tiara <victor.tiara@gmail.com>
2024-04-21 07:49:52 -0300

    dot vs bracket notion

--- fundamentos-javascript-2/dot-vs-bracket-notation.js
+++ fundamentos-javascript-2/dot-vs-bracket-notation.js
@@ -1,6 +1,5 @@
 /*
 Dot vs. Bracket Notation
-*/
 
 const jonas = {
     firstName: 'Jonas',
@@ -9,6 +8,7 @@ const jonas = {
     job: 'Teacher',
     friends: ['Michael', ' Peter', ' Steven']
 };
+
 console.log(jonas);
 
 console.log(jonas.lastName);
@@ -31,7 +31,32 @@ jonas.location = 'Portugal';
 jonas['twitter'] = '@jonasschmedtman';
 console.log(jonas);
 
+
 //Challenge
 //"Jonas has 3 friends, and his best friend is called Michael"
 
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}.`);
+*/
+
+/*
+Dot vs. Bracket Notation - 2
+
+1- Using the object from the previous assignment, log a string like this to the console: 
+'Finland has 6 million finnish-speaking people, 
+3 neighbouring countries and a capital called Helsinki'.
+
+2- Increase the country's population by two million using dot notation, 
+and then decrease it by two million using bracket notation.
+*/
+
+const finland = {
+    country: 'Finland',
+    population: 6.0,
+    capitalCity: 'Helsinki',
+    neighbors: ['Sweden', ' Russian', ' England']
+};
+
+finland.population += 2;
+finland.population -=2 ;
+
+console.log(`${finland.country} has ${finland.population} million finnish-speaking people, ${finland.neighbors} neighbouring countries and a capital called ${finland.capitalCity}.`);
