9583cd6431810cbd1dcf4a2b47fefcb45dd9fe53
Victor Tiara <victor.tiara@gmail.com>
2024-04-23 12:49:41 -0300

    object methods

--- fundamentos-javascript-2/object-methods.js
+++ fundamentos-javascript-2/object-methods.js
@@ -22,3 +22,38 @@ const jonas = {
 };
 
 console.log(jonas.getSumary());
+
+/*
+1- Add a method called describe to the myCountry object. This method will log a string to the console, 
+similar to the string logged in the previous assignment, but this time using the 'this' keyword.
+
+2- Call the describe method.
+
+3- Add a method called checkIsland to the myCountry object. 
+This method will set a new property on the object, called isIsland. 
+isIsland will be true if there are no neighbouring countries, and false if there are. Use the ternary operator to set the property.
+*/
+
+const myCountry = {
+
+    country: "Brasil",
+    capitalCity: 'Brasilia',
+    population: 210,
+    language: 'portugues',
+    neighbours: ['Argnetina', 'Uruguai', 'Chile'],
+    isIsland: true,
+
+    describe: function () {
+        console.log(`${this.country} has ${this.population} million ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capitalCity}.`);
+    },
+
+    checkIsland: function () {
+        this.isIsland = this.neighbours.length === 0 ? true : false;
+    }
+
+};
+
+myCountry.describe();
+myCountry.checkIsland();
+
+console.log(myCountry);
