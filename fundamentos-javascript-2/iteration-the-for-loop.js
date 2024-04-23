0815f18508a6aef68f25ab96ffca9ee18623d517
Victor Tiara <victor.tiara@gmail.com>
2024-04-23 12:51:33 -0300

    iteration the for loops

---
+++ fundamentos-javascript-2/iteration-the-for-loop.js
@@ -0,0 +1,13 @@
+/*
+Iteration: The for Loop
+
+1- There are elections in your country! in a small town, 
+there are only 50 voters. Use a for loop to simulate the 50 people voting, 
+by logging a string like this to the console (for numbers 1 to 50): 
+'Voter number 1 is currently voting'.
+*/
+
+// Para o loop funcionar a condição tem que ser TRUE.
+for (let voter = 1; voter <= 50; voter++) {
+    console.log(`Voter number ${voter} is currently voting'`);
+}
